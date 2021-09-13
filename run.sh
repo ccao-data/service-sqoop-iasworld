a8c6a3bd2209b43c6d1836cb6ee471e6d6eacada
Dan Snow <daniel.snow@cookcountyil.gov>
2021-09-12 23:14:35 -0500

    Remove cruft from entrypoint script

--- run.sh
+++ run.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-# for table in tables
-
-# if [! -f jobs/
-
-# # 1. Get list of table names
-
-# # 2. Run in loop with compose
-
-# # 3. aws s3 sync target dir
-
 docker-compose run --rm sqoop /scripts/create_jobs.sh
 docker-compose run --rm sqoop /scripts/run_jobs.sh
