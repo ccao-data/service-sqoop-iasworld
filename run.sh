53183213185792bd7da9d5a2daf9166996fe7232
Dan Snow <daniel.snow@cookcountyil.gov>
2021-09-13 23:09:11 -0500

    Add S3 syncing logic

--- run.sh
+++ run.sh
@@ -1,3 +1,33 @@
 #!/bin/bash
-#docker-compose -f create-jobs.yaml up
-docker-compose -f run-jobs.yaml up --abort-on-container-exit
+
+# Name for log file. Since this is a daily export, we want one for each day
+LOG_FILE="logs/$(date +%Y-%m-%d)-sqoop-log.txt"
+
+# Create sqoop jobs. These are saved in the metastore/ directory
+docker-compose -f create-jobs.yaml up \
+    | tee ${LOG_FILE}
+
+# Run all sqoop jobs. Parquet files are output to target/ directory
+docker-compose -f run-jobs.yaml up --abort-on-container-exit \
+    | tee -a ${LOG_FILE}
+
+# Sync metastore/ directory to S3
+aws s3 sync \
+    metastore/ \
+    s3://dev-ccap-athenapoc-landingzone-us-east1/iasworld/metastore \
+    --exclude "*gitkeep" \
+    | tee -a ${LOG_FILE}
+
+# Sync target/ directory to S3
+aws s3 sync \
+    target/ \
+    s3://dev-ccap-athenapoc-landingzone-us-east1/iasworld/data \
+    --exclude "*" \
+    --include "*.parquet" \
+    | tee -a ${LOG_FILE}
+
+# Dump log file to S3 after compressing
+aws s3 sync \
+    logs/ \
+    s3://dev-ccap-athenapoc-landingzone-us-east1/iasworld/logs \
+    --exclude "*gitkeep" \
