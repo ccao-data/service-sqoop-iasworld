b68a7956ec69822f01cf77e6abeff19cb6b54c30
Dan Snow <daniel.snow@cookcountyil.gov>
2021-09-14 00:02:23 -0500

    Checkpoint

--- run.sh
+++ run.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 # Name for log file. Since this is a daily export, we want one for each day
-LOG_FILE="logs/$(date +%Y-%m-%d)-sqoop-log.txt"
+TEMP_LOG_FILE="logs/temp-sqoop-log.txt"
 
 # Create sqoop jobs. These are saved in the metastore/ directory
 docker-compose -f create-jobs.yaml up \
-    | tee ${LOG_FILE}
+    | tee ${TEMP_LOG_FILE}
 
 # Run all sqoop jobs. Parquet files are output to target/ directory
 docker-compose -f run-jobs.yaml up --abort-on-container-exit \
-    | tee -a ${LOG_FILE}
+    | tee -a ${TEMP_LOG_FILE}
 
 # Sync metastore/ directory to S3
 aws s3 sync \
     metastore/ \
     s3://dev-ccap-athenapoc-landingzone-us-east1/iasworld/metastore \
     --exclude "*gitkeep" \
-    | tee -a ${LOG_FILE}
+    | tee -a ${TEMP_LOG_FILE}
 
 # Sync target/ directory to S3
 aws s3 sync \
@@ -24,9 +24,11 @@ aws s3 sync \
     s3://dev-ccap-athenapoc-landingzone-us-east1/iasworld/data \
     --exclude "*" \
     --include "*.parquet" \
-    | tee -a ${LOG_FILE}
+    | tee -a ${TEMP_LOG_FILE}
 
-# Dump log file to S3 after compressing
+# Dump log file to S3 after compressing to zstd
+LOG_FILE="logs/$(date +%Y-%m-%d)-sqoop-log.txt"
+zstd --rm ${TEMP_LOG_FILE} -o ${LOG_FILE}.zst
 aws s3 sync \
     logs/ \
     s3://dev-ccap-athenapoc-landingzone-us-east1/iasworld/logs \
