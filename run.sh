c80683036af722bfc1674ac1da59f87fd43d9628
Dan Snow <snow.dfs@gmail.com>
2021-09-21 19:56:28 +0000

    Add cronjob to run export at 1 AM each morning

--- run.sh
+++ run.sh
@@ -5,27 +5,27 @@ TEMP_LOG_FILE="logs/temp-sqoop-log.txt"
 BUCKET_URI="s3://ccao-landing-us-east-1"
 
 # Create sqoop jobs. These are saved in the metastore/ directory
-docker-compose \
+/usr/local/bin/docker-compose \
     -f create-jobs.yaml up \
     --remove-orphans \
     | tee ${TEMP_LOG_FILE}
 
 # Run all sqoop jobs. Parquet files are output to target/ directory
-docker-compose \
+/usr/local/bin/docker-compose \
     -f run-jobs.yaml up --abort-on-container-exit \
     --remove-orphans \
     | tee -a ${TEMP_LOG_FILE}
 
 # Sync metastore/ directory to S3
 # Need to keep a local copy in order to save state
-aws s3 sync \
+/usr/bin/aws s3 sync \
     metastore/ \
     ${BUCKET_URI}/iasworld/metastore \
     --exclude "*gitkeep" \
     | tee -a ${TEMP_LOG_FILE}
 
 # Move target/ directory to S3
-aws s3 mv \
+/usr/bin/aws s3 mv \
     target/ \
     ${BUCKET_URI}/iasworld/data \
     --exclude "*" \
@@ -35,8 +35,8 @@ aws s3 mv \
 
 # Move log file to S3 after compressing to zstd
 LOG_FILE="logs/$(date -u -Iseconds)-sqoop-log.txt"
-zstd -f --rm ${TEMP_LOG_FILE} -o ${LOG_FILE}.zst
-aws s3 mv \
+/usr/bin/zstd -f --rm ${TEMP_LOG_FILE} -o ${LOG_FILE}.zst
+/usr/bin/aws s3 mv \
     logs/ \
     ${BUCKET_URI}/iasworld/logs \
     --exclude "*gitkeep" \
