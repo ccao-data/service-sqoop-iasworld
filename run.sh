747a676033b2e01879e53f9780a4b6716ed7ca63
Dan Snow <snow.dfs@gmail.com>
2021-09-27 19:41:39 +0000

    Upload sqoop logs to AWS CloudWatch

--- run.sh
+++ run.sh
@@ -1,19 +1,25 @@
 #!/bin/bash
 
-# Name for log file. Since this is a daily export, we want one for each day
-TEMP_LOG_FILE="logs/temp-sqoop-log.txt"
+# Env variables controlling where sqooped data is exported to
+TEMP_LOG_FILE="logs/temp-sqoop-log"
 BUCKET_URI="s3://ccao-landing-us-east-1"
+LOG_GROUP_NAME="/ccao/jobs/sqoop"
 
 # Create sqoop jobs. These are saved in the metastore/ directory
+# and synced to S3 on job completion
 /usr/local/bin/docker-compose \
     -f create-jobs.yaml up \
     --remove-orphans \
+    --no-color \
+    | ts '%.s' \
     | tee ${TEMP_LOG_FILE}
 
 # Run all sqoop jobs. Parquet files are output to target/ directory
 /usr/local/bin/docker-compose \
     -f run-jobs.yaml up --abort-on-container-exit \
     --remove-orphans \
+    --no-color \
+    | ts '%.s' \
     | tee -a ${TEMP_LOG_FILE}
 
 # Sync metastore/ directory to S3
@@ -22,6 +28,8 @@ BUCKET_URI="s3://ccao-landing-us-east-1"
     metastore/ \
     ${BUCKET_URI}/iasworld/metastore \
     --exclude "*gitkeep" \
+    --no-progress \
+    | ts '%.s' \
     | tee -a ${TEMP_LOG_FILE}
 
 # Move target/ directory to S3
@@ -31,13 +39,35 @@ BUCKET_URI="s3://ccao-landing-us-east-1"
     --exclude "*" \
     --include "*.parquet" \
     --recursive \
+    --no-progress \
+    | ts '%.s' \
     | tee -a ${TEMP_LOG_FILE}
 
-# Move log file to S3 after compressing to zstd
-LOG_FILE="logs/$(date -u -Iseconds)-sqoop-log.txt"
-/usr/bin/zstd -f --rm ${TEMP_LOG_FILE} -o ${LOG_FILE}.zst
-/usr/bin/aws s3 mv \
-    logs/ \
-    ${BUCKET_URI}/iasworld/logs \
-    --exclude "*gitkeep" \
-    --recursive
+# Convert text output from Docker and AWS CLI to clean JSON
+# for upload to AWS CloudWatch
+LOG_FILE="logs/$(date -u -Iseconds)-sqoop-log.json"
+LOG_STREAM_NAME="sqoop-job-log-$(date -u +'%Y-%m-%d_%H-%M-%S')"
+cat ${TEMP_LOG_FILE} \
+    | sed 's/ /|/' \
+    | sed 's/\([0-9]\.[0-9]\{3\}\)[0-9]\{1,\}/\1/' \
+    | sed 's/\.//' \
+    | awk '{print $1, $NF}' OFS='|' FS='|' \
+    | jq -Rn '[inputs
+        | . / "\n"
+        | (.[] | select(length > 0) | . / "|") as $input
+        | {"timestamp": $input[0]|tonumber, "message": $input[1]}]' \
+    > ${LOG_FILE}
+
+# Create log stream in CloudWatch with today's date
+/usr/bin/aws logs create-log-stream \
+    --log-group-name ${LOG_GROUP_NAME} \
+    --log-stream-name ${LOG_STREAM_NAME}
+
+# Upload logs to CloudWatch
+/usr/bin/aws logs put-log-events \
+    --log-group-name ${LOG_GROUP_NAME} \
+    --log-stream-name ${LOG_STREAM_NAME} \
+    --log-events file://${LOG_FILE}
+
+# Remove temp files
+rm ${LOG_FILE} ${TEMP_LOG_FILE}
